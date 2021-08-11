c666446c50e2a3eb462c008ac6af21ff46b83bd8
Heng Yuan <superduperhengyuan@gmail.com>
2021-08-10 22:52:59 -0700

    Update pom for the project and build.cmd

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
 @echo off
-mvn clean package -Dmaven.test.skip=true
+call mvn clean package -Dmaven.test.skip=true
 copy /y jaqy-avro\target\jaqy-avro*.jar dist & copy /y jaqy-s3\target\jaqy-s3*.jar dist & copy /y jaqy-azure\target\jaqy-azure*.jar dist
